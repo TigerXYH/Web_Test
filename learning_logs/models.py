52ba8a4cf4d4918f56d1798197964b270cdaed00
TigerX <ftha@qq.com>
2019-04-11 11:25:12 +0800

    Enter

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # 在这里创建模型 Create your models here. 
 
-class Topic(models):
+class Topic(models.Model):
     """ 用户学习的主题 """
     text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=Ture)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ 返回模型的字符串表示 """
