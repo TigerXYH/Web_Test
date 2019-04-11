4635ff10c23008d6b3724d022062de98c72fd9fa
TigerX <ftha@qq.com>
2019-04-11 17:51:14 +0800

    enter

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# 在这里注册你的模型 Register your models here.
+
+from learning_logs.models import Topic, Entry
+
+admin.site.register(Topic)
+admin.site.register(Entry)
