6e334cae25cd7d3af0517a300abceff8a8d72ca7
X <ftha@qq.com>
2019-04-15 08:08:59 +0800

    enter

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+# 在这里创建视图 Create your views here.
+
+def index(request):
+    """ 学习笔记的主页 """
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ 显示所有主题 """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
