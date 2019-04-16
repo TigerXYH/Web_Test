60ba35eb09e669a4ea57037be0c8b79d8c9fe718
TigerX <ftha@qq.com>
2019-04-16 16:56:47 +0800

    enter

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # 在这里创建视图 Create your views here.
 
@@ -40,5 +40,25 @@ def new_topic(request):
             form.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
-    conetext = {'form': form}
+    context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """ 在特定的主题中添加新条目 """
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry. save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
