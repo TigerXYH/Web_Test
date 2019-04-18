e7c05780fa4a07cf826a0c01d1a9c055b09d943d
X <ftha@qq.com>
2019-04-18 08:11:27 +0800

    enter

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,18 @@
 """ 为应用程序users定义URL模式 """
 
-from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
+
 
 app_name = 'users'
 
 urlpatterns = [
     # 登录页面
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # 注销
+    path('logout/', LogoutView.as_view(), name='logout'),
+    #path('logout/' views.LogoutView, name='logout'),
 ]
-    
